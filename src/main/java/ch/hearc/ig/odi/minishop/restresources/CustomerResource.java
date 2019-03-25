df01c896155e5b2c649107d983331609b813bd0c
MajidFatima <fatima.majid@he-arc.ch>
2019-03-25 21:04:24 +0100

    GET AllCustomers
    getAllCustomers

--- src/main/java/ch/hearc/ig/odi/minishop/restresources/CustomerResource.java
+++ src/main/java/ch/hearc/ig/odi/minishop/restresources/CustomerResource.java
@@ -4,9 +4,12 @@
 
 package ch.hearc.ig.odi.minishop.restresources;
 
+import ch.hearc.ig.odi.minishop.business.Customer;
 import ch.hearc.ig.odi.minishop.services.PersistenceService;
+import java.util.List;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -18,5 +21,10 @@ public class CustomerResource {
 
   @Inject
   private PersistenceService persistenceService;
+
+  @GET
+  public List<Customer> getCustomerList() {
+    return persistenceService.getAllCustomers();
+  }
 }
 
