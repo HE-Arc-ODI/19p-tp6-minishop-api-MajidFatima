598086cd2b3693fccf9ad77f658799a17073ae35
MajidFatima <fatima.majid@he-arc.ch>
2019-03-26 08:59:27 +0100

    Update CustomerResource.java
    Add PUT customer/1

--- src/main/java/ch/hearc/ig/odi/minishop/restresources/CustomerResource.java
+++ src/main/java/ch/hearc/ig/odi/minishop/restresources/CustomerResource.java
@@ -5,12 +5,16 @@
 package ch.hearc.ig.odi.minishop.restresources;
 
 import ch.hearc.ig.odi.minishop.business.Customer;
+import ch.hearc.ig.odi.minishop.exception.CustomerException;
+import ch.hearc.ig.odi.minishop.exception.NullFormException;
 import ch.hearc.ig.odi.minishop.services.PersistenceService;
 import java.util.List;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -25,6 +29,18 @@ public class CustomerResource {
   @GET
   public List<Customer> getCustomers() {
     return persistenceService.getAllCustomers();
+  }
+
+  @PUT
+  @Path("{id}")
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Customer updateCustomer(@PathParam("id") Long id, Customer customer) {
+    try {
+      return persistenceService.updateCustomer(id, customer);
+    } catch (CustomerException e) {
+      e.printStackTrace();
+      throw new NullFormException("customer couldn't have been updated.");
+    }
   }
 }
 
