7a8067c88c1e455ad7e99ab665fa72ac5cc4e080
MajidFatima <fatima.majid@he-arc.ch>
2019-03-30 22:51:10 +0100

    getCustomerByID

--- src/main/java/ch/hearc/ig/odi/minishop/restresources/CustomerResource.java
+++ src/main/java/ch/hearc/ig/odi/minishop/restresources/CustomerResource.java
@@ -6,16 +6,19 @@ package ch.hearc.ig.odi.minishop.restresources;
 
 import ch.hearc.ig.odi.minishop.business.Customer;
 import ch.hearc.ig.odi.minishop.exception.CustomerException;
+import ch.hearc.ig.odi.minishop.exception.NotFoundException;
 import ch.hearc.ig.odi.minishop.exception.NullFormException;
 import ch.hearc.ig.odi.minishop.services.PersistenceService;
 import java.util.List;
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 @Path("customer")
@@ -25,21 +28,28 @@ public class CustomerResource {
 
   @Inject
   private PersistenceService persistenceService;
-
+/*
+Pour la ressource GET /customer, il faut que la ressources
+retourne un tableaux d'objets JSON (MediaType: application/json), voici un exemple :
+*/
   @GET
   public List<Customer> getCustomers() {
     return persistenceService.getAllCustomers();
   }
 
-  @PUT
+  /*
+  Pour la ressource GET /customer/1, il faut que la ressources retourne
+   un objet JSON (MediaType: application/json), voici un exemple :
+   */
+
+  @GET
   @Path("{id}")
-  @Consumes(MediaType.APPLICATION_JSON)
-  public Customer updateCustomer(@PathParam("id") Long id, Customer customer) {
+  public Customer getCustomer(@Context HttpServletRequest servletRequest, @PathParam("id") Long id) {
     try {
-      return persistenceService.updateCustomer(id, customer);
+      return persistenceService.getCustomerByID(id);
     } catch (CustomerException e) {
       e.printStackTrace();
-      throw new NullFormException("customer couldn't have been updated.");
+      throw new NotFoundException("the customer does not exist.");
     }
   }
 }
